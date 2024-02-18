9b8d4fd8f00c7dde3c55c8553336e1c418532298
jeanj777 <135941076+jeanj777@users.noreply.github.com>
2024-02-18 16:17:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '0064fa86-fe09-45ef-9b8d-34fe0d3b74a4-00-qojubmny0m0v.janeway.replit.dev'
+      url.hostname = '1e35217d-a702-4ef7-8b87-7b932bd52df1-00-1fcrdgeksl8az.kirk.replit.dev'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
